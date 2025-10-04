6b6f789320ca274aae5df2720a3aa7294d13b4b8
nelioRamos1 <nelioramos642@gmail.com>
2025-10-04 13:42:30 -0300

    Reformulação do carrossel e parte inicial (base) pronta

--- Home/script.js
+++ Home/script.js
@@ -1,4 +1,13 @@
-function clickMenu() {
-    const menu = document.getElementById('itens');
-    menu.classList.toggle('active');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelector('.slides');
+    const images = document.querySelectorAll('.slides picture');
+    let currentIndex = 0;
+    
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % images.length;
+        slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+    
+    setInterval(nextSlide, 10
+        000);
+});
